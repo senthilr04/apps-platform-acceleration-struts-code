5b7ff3b02d96f32164705cec9d7c48ee045a8e0f
senthilr04 <senthilkumar.rajamanickam2@cognizant.com>
2018-01-25 17:55:48 -0700

    Struts to Spring

--- src/main/java/org/superbiz/struts/FindUser.java
+++ src/main/java/org/superbiz/struts/FindUser.java
@@ -17,11 +17,21 @@
  */
 package org.superbiz.struts;
 
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import java.util.Properties;
 
+@Component
 public class FindUser {
+
+    private final UserService service;
+
+    public FindUser(UserService service) {
+        this.service = service;
+    }
 
     private int id;
     private String errorMessage;
@@ -51,15 +61,10 @@ public class FindUser {
         this.id = id;
     }
 
+    @Transactional
     public String execute() {
 
         try {
-            UserService service = null;
-            Properties props = new Properties();
-            props.put(Context.INITIAL_CONTEXT_FACTORY,
-                "org.apache.openejb.core.LocalInitialContextFactory");
-            Context ctx = new InitialContext(props);
-            service = (UserService) ctx.lookup("UserServiceImplLocal");
             this.user = service.find(id);
         } catch (Exception e) {
             this.errorMessage = e.getMessage();
